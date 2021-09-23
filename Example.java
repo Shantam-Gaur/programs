617b3c7ee67c6f6f344cd6daf5ff23595a2712f4
Shantam-Gaur <90915510+Shantam-Gaur@users.noreply.github.com>
2021-09-24 01:03:53 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,15 @@
+class Example
+{
+   static void func()
+   {
+        System.out.println("function working");
+   }
+   public static void  main(String args[])
+   {
+        Example.func();
+        Example obj=new Example();
+        obj.func();
+    
+   }
+    
+}
