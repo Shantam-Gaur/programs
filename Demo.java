f8ac7082fe06d2cb559313d36f0fef50cb26eac6
Shantam-Gaur <90915510+Shantam-Gaur@users.noreply.github.com>
2021-09-24 01:03:33 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,12 @@
+class Demo
+{
+          void func()
+          {
+                     System.out.print("first program...");
+          }
+          public static void main(String args[])
+          {
+                     Demo obj=new Demo();
+                     obj.func();
+          }
+}
